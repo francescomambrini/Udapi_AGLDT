2161dbef4d9dbc14d41839a0e7c70122869431e5
francescomambrini <francescomambrini@users.noreply.github.com>
2021-11-07 21:32:12 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
    description='A collection of blocks for the Udapi Python framework, created to provide support for the AGLDT treebanks',
    author='Francesco Mambrini',
    author_email='',
-   packages=['udapi_agldt'],
+   packages=['udapi_agldt', 'udapi_agldt.read'],
    install_requires=['udapi', 'lxml']
 )
