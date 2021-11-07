430bdb8f3b2c39a50da18177f10be1835c892284
francescomambrini <francescomambrini@users.noreply.github.com>
2021-11-07 21:12:58 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='udapi_agldt',
+   version='0.1',
+   description='A collection of blocks for the Udapi Python framework, created to provide support for the AGLDT treebanks',
+   author='Francesco Mambrini',
+   author_email='',
+   packages=['udapi_agldt'],
+   install_requires=['udapi', 'lxml']
+)
